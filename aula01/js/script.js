77111ec96dac04dd5b876b00fd31d504e2a92bc0
Joao Sone <sousa.joao.neto@gmail.com>
2017-04-11 17:03:52 -0300

    Remoção do evento de carregamento

--- aula01/js/script.js
+++ aula01/js/script.js
@@ -20,9 +20,8 @@ $(document).ready(function(){
 	/* END Formlulario */
 	
 	/* BEGIN jQueryAjax */
-	$("#driver").click(function(event){
-		$('#stage').load('conteudo.html');
-	});
+	/*div driver(btn) função load()*/
+	
 	/* END jQueryAjax */
 	
 });
