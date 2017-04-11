afa9cc58cae2c3160f431c50b0d0a014245c09ac
Joao Sone <sousa.joao.neto@gmail.com>
2017-04-11 11:47:50 -0300

    Adicionando Aula01 e Aula02
    Aula 01: forulario de upload de arquivo com php + uso de jquery com ajax
    load() para alterar o conteudo da página.
    Aula 02: uso de jquery ajax get() para alterar o conteudo da pagina +
    menu dropdown com jquery

---
+++ aula01/enviar.php
@@ -0,0 +1,29 @@
+<?php
+	if(isset($_POST['pasta']) && $_POST['pasta']){
+		
+		$pasta_dir = $_POST["pasta"];
+		$file = $_FILES["arquivo"];
+		if(!file_exists($pasta_dir))
+			mkdir($pasta_dir);
+		$nome = $file["name"];
+		$tamanho = $file["size"];
+		$tipo = $file["type"];	
+		$tmpName = $file["tmp_name"];	
+		$erro = $file["error"];
+		
+		//valid
+		if($erro == 0){
+			$novoNome = date("Ymd").rand(1,9999)."-".$nome;
+			$caminho = $pasta_dir."/".$novoNome;
+			
+			if(move_uploaded_file($tmpName, $caminho)){
+				echo "Upload realizado com sucesso para:<br>".$caminho;
+			}else{
+				echo $erro;
+			}
+			
+		}else{
+			echo $erro;
+		}
+	}
+?>
---
+++ aula01/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	
+	/* BEGIN Formlulario */
+	$("#form1").on("submit", function(e){
+		//e.preventDefault();
+	});
+	
+	$(".btn1").on("click",function(){
+		$(".msg").removeClass("erro");
+		$(".msg").addClass("sucesso");
+	});
+	$(".btn2").on("click",function(){
+		$(".msg").removeClass("sucesso");
+		$(".msg").addClass("erro");
+	});
+	$(".btn3").on("click",function(){		
+		$(".msg").removeClass("sucesso erro");
+		$(".msg").toggleClass("active");
+	});
+	/* END Formlulario */
+	
+	/* BEGIN jQueryAjax */
+	$("#driver").click(function(event){
+		$('#stage').load('conteudo.html');
+	});
+	/* END jQueryAjax */
+	
+});
